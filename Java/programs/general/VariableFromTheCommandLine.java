3d59a6ab84e4b2851345f548ed662b0572514482
Suresh Sehrawat <jaisehrawat11@gmail.com>
2024-06-21 12:41:52 +0530

    A-Suresh Sehrawat | Java | Programs | general | Variable from command line

---
+++ Java/programs/general/VariableFromTheCommandLine.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class VariableFromTheCommandLine {
+    public static void main(String args[]){
+        System.out.println(Arrays.toString(args));
+        System.out.println(args.length);
+    }
+}
